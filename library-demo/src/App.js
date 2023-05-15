ad140105434bbddee745d86100035362a1ab2f5b
Misael Michel <develop@tiecode.net>
2023-05-15 15:42:43 -0400

    chage

--- library-demo/src/App.js
+++ library-demo/src/App.js
@@ -5,7 +5,7 @@ import { Navbar, Nav, Button } from 'react-bootstrap';
 import './App.css';
 
 import BookList from './Components/BookList/BookList';
-import AuthorList from './Components/AuthorList/AuthorList';
+// import AuthorList from './Components/AuthorList/AuthorList';
 
 const client = new ApolloClient({
   uri: '/data-api/graphql',
